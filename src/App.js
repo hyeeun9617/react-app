4ab8eab14a2b92e7dc39b628437005d76eac2ef5
hyeeun <hyeeun9617@naver.com>
2020-06-14 11:11:32 +0900

    환경설정

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//현재 웹사이트 화면에 표기되는 정보를 담고 있다.
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1 className="App-title">리액트 세계에 오신거를 환영합니다. (예시 작성)</h1> 
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
